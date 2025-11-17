8f99ede73f924b0ace46008326368c0c0bbd8faa
USMAN AHMAD <usmaneng220@gmail.com>
2025-11-17 22:14:38 +0500

    hero section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './App.css';
-import NavbarComponent from './components/Navbar/Navbar'; // Yahan import karein
+import NavbarComponent from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero'; // Hero component import karein
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
-
-      {/* Hero Section ya baaqi content yahan ayega */}
-      {/* Hum ne neeche space daala hai taake scroll karke effect dekh sakein */}
-      <header className="App-header">
-        <h1>Scroll Down to see Navbar Effect</h1>
-        <p>Your content starts here.</p>
-      </header>
+      <Hero />
+      {/* Baaqi website ke sections yahan neeche ayeinge */}
     </div>
   );
 }
